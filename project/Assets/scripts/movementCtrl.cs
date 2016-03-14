a62e818b0a0d7d04ff8314eff1ae345f98c5278f
Axel <axelakakwel@gmail.com>
2016-03-14 02:05:28 +0100

    Script for movements

--- project/Assets/scripts/movementCtrl.cs
+++ project/Assets/scripts/movementCtrl.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class character : MonoBehaviour {
+public class movementCtrl : MonoBehaviour {
+
+	public float maxSpeed = 10.0f;
+
+	bool facingRight = true;
+
+	Animator anim;
+	Rigidbody2D rigidbody;
 
 	// Use this for initialization
 	void Start () {
-	
+		anim = GetComponent<Animator>();	
+		rigidbody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		float move = Input.GetAxis("Horizontal");
+		anim.SetFloat("speed", Mathf.Abs(move));
+		rigidbody.velocity = new Vector2(move * maxSpeed, rigidbody.velocity.y);
+
+		if (move > 0 && !facingRight) {
+			FlipFacing();
+		}
+		else if (move < 0 && facingRight) {
+			FlipFacing();
+		}
+	}
+
+	void FlipFacing() {
+		facingRight = !facingRight;
+		Vector3 charScale = transform.localScale;
+		charScale.x *= -1;
+		transform.localScale = charScale;
 	}
 }
