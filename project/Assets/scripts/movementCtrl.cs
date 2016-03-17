266390c517ede84804a279983fbd481d965705cf
Axel <axelakakwel@gmail.com>
2016-03-17 01:09:07 +0100

    jump animation placeholder

--- project/Assets/scripts/movementCtrl.cs
+++ project/Assets/scripts/movementCtrl.cs
@@ -3,30 +3,51 @@ using System.Collections;
 
 public class movementCtrl : MonoBehaviour {
 	public float maxSpeed = 10.0f;
+	public float jumpSpeed = 10.0f;
+
+	//Empty gameobject created to determine the bounds/center of the object
+	public Transform GroundCheck1;
+
+	public LayerMask ground_layers;
 
 	bool facingRight = true;
 
 	Animator anim;
-	Rigidbody2D rigidbody;
+	Rigidbody2D rigidBody;
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator>();	
-		rigidbody = GetComponent<Rigidbody2D>();
+		rigidBody = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		float move = Input.GetAxis("Horizontal");
-		anim.SetFloat("speed", Mathf.Abs(move));
-		float absVelocityX = Mathf.Abs(move) * maxSpeed;
-		float velocityX = facingRight ? absVelocityX : -absVelocityX;
-		rigidbody.velocity = new Vector2(velocityX, rigidbody.velocity.y);
 
-		if ((move > 0 && !facingRight) || (move < 0 && facingRight)) {
-			//anim.SetTrigger("flip"); // run break
+	//All the Physics related things to be done here
+	void FixedUpdate(){
+		bool isGrounded = IsGrounded();
+		float moveX = Input.GetAxisRaw("Horizontal");
+		float moveY = Input.GetAxisRaw("Vertical");
+		float absVelocityX = Mathf.Abs(moveX) * maxSpeed;
+
+		float velocityX = facingRight ? absVelocityX : -absVelocityX;
+		float velocityY = rigidBody.velocity.y;
+
+		anim.SetFloat("absMoveX", absVelocityX);
+		anim.SetFloat("moveY", velocityY);
+		anim.SetBool("grounded", isGrounded);
+
+		if ((moveX > 0 && !facingRight) || (moveX < 0 && facingRight)) {
 			FlipFacing();
 		}
+
+		if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+			velocityY = jumpSpeed;
+		}
+
+		rigidBody.velocity = new Vector2(velocityX, velocityY);
+	}
+
+	bool IsGrounded() {
+		return Physics2D.OverlapCircle(GroundCheck1.position, 0.1f, ground_layers);
 	}
 
 	void FlipFacing() {
